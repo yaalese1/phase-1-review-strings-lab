a6675ebd9e2e49727a9c669a63fcb8d11fb78fba
Yetunde Alese <yaalese1@gmail.com>
2022-11-02 18:23:37 -0400

    commit tree 12a8496f0a45aca53cd86953783f870eb25705b7

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper"
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+ const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!` 
+
+// const myLength = currentUser.length
+// console.log(myLength)
